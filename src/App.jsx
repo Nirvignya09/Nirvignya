7c80d6f237356ad7cb1f9be891f5cdeab59151aa
Nirvignya09 <ageernirvignya@gmail.com>
2024-12-03 18:03:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import ContactPage from "./components/ContactPage";
+import ContactPage from "./components/Contactpage";
 
 function App() {
   return (
